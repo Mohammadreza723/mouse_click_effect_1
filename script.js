e2a62822d1a17ee320fda8a7dad26d7444832c59
Mohammadreza <hozoorimohammadreza1@gmail.com>
2024-01-15 00:53:56 +0330

    add animation to shadow

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const mouse = document.querySelector(".mouse_bg")
+document.addEventListener("DOMContentLoaded", () => {
+  const mouse = document.querySelector(".mouse_bg");
+  const sec_shadow = document.querySelector(".sec_shadow");
 
-window.addEventListener("mousemove", (e) => {
-    mouse.style.top = e.clientY * 1 - 45 + "px";
-    mouse.style.left = e.clientX * 1 - 45 + "px";
-})
+  window.addEventListener("mousemove", (e) => {
+    mouse.style.top = e.clientY - 45 + "px";
+    mouse.style.left = e.clientX - 45 + "px";
+  });
 
-window.addEventListener("click", (e) => {
-    console.log(e);
-})
+  window.addEventListener("click", (e) => {
+    isclicked = true;
+    sec_shadow.classList.add("active");
+    setTimeout(() => {
+      sec_shadow.classList.remove("active");
+    }, 500);
+  });
+});
